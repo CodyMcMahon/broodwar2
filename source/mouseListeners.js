3e32a9e50f7ed7385f59faa07e0ed04c2788c005
CodyMcMahon <codyjamesmcmahon@gmail.com>
2016-06-16 23:33:10 -0700

    idk man programming is hard osmetimes

--- source/mouseListeners.js
+++ source/mouseListeners.js
@@ -100,7 +100,9 @@ function mouseUp(e){
         }
       }
       //alert(u.length);
-      me.selection||me.selection[0].portraitn = 0;
+    if(me.selection){
+      me.selection[0].portraitn = 0;
+    }
       me.selection = u;
     }
     me.isOnGameScreen = 0;
